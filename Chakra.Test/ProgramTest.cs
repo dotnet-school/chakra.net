12f1c6d5bb297fe2dc807e2f587d2e52a87139c3
nishant <nishant.singh87@gmail.com>
2020-10-06 21:16:49 +0530

    Should show correct compile error for programs.

--- Chakra.Test/ProgramTest.cs
+++ Chakra.Test/ProgramTest.cs
@@ -27,6 +27,32 @@ namespace Chakra.Test
             ExpectOutput(snippet, "Hello", "World !");
         }
      
+        [Fact]
+        public void ShouldReportCompileErrorInProgram()
+        {
+            string snippet =  @"
+            using System;
+            namespace Client
+            {
+                class Program
+                {
+                    static void Main(string[] args)
+                    {
+                        Console.WriteLine(""Hello"");
+                        Console.WriteLine(""World !"");
+                        Console.WriteLine(undefined);                        
+                    }
+                }
+            }";
+
+            string expectedMessage = "CS0103: The name 'undefined' does not exist in the current context";
+            int expectedLine = 10; 
+            DynamicCompilationException exception = Assert.Throws<DynamicCompilationException>(
+                            () => Executor.Execute(snippet));
+
+            Assert.Equal(expectedMessage, exception.Message);
+            Assert.Equal(expectedLine, exception.LineNumber);
+        }
         
         private void ExpectOutput(string snippet, params string[] expected)
         {
