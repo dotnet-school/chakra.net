31fc6c4d06488d4c176cc8fbb6c5b31412840925
nishant <nishant.singh87@gmail.com>
2020-10-06 19:54:46 +0530

    Add a client project for debugging.

---
+++ Chakra.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chakra.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] snippet =  @"
+            int value = 34;
+            Assert.Equal(3 , -1);".Split(Environment.NewLine);
+
+            string result = Executor.ExecuteSnippet(snippet);
+
+            Console.WriteLine(result);
+        }
+
+        // private static string Run()
+        // {
+        //     BreakLines(snippet)
+        // }
+    }
+}
